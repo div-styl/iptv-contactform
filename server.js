6bf74cd8b680fa501d81979ea9bb099af68af79c
div-styl <gouhmada@gmail.com>
2023-10-25 00:00:33 +0100

    idk yet

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ const emailconfig = nodemailer.createTransport({
     user: USER_EMAIL, // replace with your email
     pass: USER_PASS, // replace with your password or use an app password
   },
-
   secure: true,
 });
 
@@ -29,8 +28,7 @@ app.use(
     ],
     credentials: true,
     optionsSuccessStatus: 200,
-  })
-);
+  });
 app.options("/Contact", cors()); // Handle preflight requests
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -41,6 +39,33 @@ app.post("/", (req, res) => {
   const { full_name, email, device_type, device_name, plan, message } =
     req.body;
   console.log(req.body);
+
+  // Setup email data
+  const mailOptions = {
+    from: USER_EMAIL,
+    to: USER_EMAIL,
+    subject: "New Order", // Change this subject as needed
+    html: `
+      <p style="font-size: 20px;">Full Name: ${full_name}</p>
+      <p style="font-size: 16px;">Email: <span style="font-weight: bold;">${email}</span></p>
+      <p style="font-size: 16px;">Device Type: <span style="font-weight: bold;">${device_type}</span></p>
+      <p style="font-size: 16px;">Device Name: <span style="font-weight: bold;">${device_name}</span></p>
+      <p style="font-size: 16px;">Plan: <span style="font-weight: bold;">${plan}</span></p>
+      <p style="font-size: 16px;">Message: <span style="font-weight: bold; color: #e74c3c;">${message} </span></p>
+    `,
+  };
+
+  // Send the email
+  emailconfig.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error("Error sending email:", error);
+      res.status(500).send("Error sending email");
+    } else {
+      console.log("Message sent: %s", info.messageId);
+      res.header("Access-Control-Allow-Credentials", true);
+      res.send("Message sent successfully");
+    }
+  });
 });
 
 // Contact form endpoint
@@ -48,26 +73,27 @@ app.post("/Contact", (req, res) => {
   const { first_name, last_name, email, subject, message } = req.body;
 
   // Setup email data
-  let mailOptions = {
+  const mailOptions = {
     from: USER_EMAIL,
     to: USER_EMAIL,
     subject: `${subject}`,
-    html: `<p style="font-size: 20px;">Full Name: ${first_name} ${last_name}</p>
-           <p style="font-size: 16px;">Email: <span style="font-weight: bold;">${email}</span></p>
-           <p style="font-size: 16px;">Message: <span style="font-weight: bold; color: #e74c3c;">${message} </span></p>`,
+    html: `
+      <p style="font-size: 20px;">Full Name: ${first_name} ${last_name}</p>
+      <p style="font-size: 16px;">Email: <span style="font-weight: bold;">${email}</span></p>
+      <p style="font-size: 16px;">Message: <span style="font-weight: bold; color: #e74c3c;">${message} </span></p>
+    `,
   };
 
-  // Send mail with defined transport object
+  // Send the email
   emailconfig.sendMail(mailOptions, (error, info) => {
     if (error) {
-      return console.log(error);
+      console.error("Error sending email:", error);
+      res.status(500).send("Error sending email");
+    } else {
+      console.log("Message sent: %s", info.messageId);
+      res.header("Access-Control-Allow-Credentials", true);
+      res.send("Message sent successfully");
     }
-    console.log("Message sent: %s", info.messageId);
-
-    // Add this line to enable credentials in CORS
-    res.header("Access-Control-Allow-Credentials", true);
-
-    res.send("Message sent successfully");
   });
 });
 
